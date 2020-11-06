b03bae1daff9f5dabeda80eccbda5e266c847c4e
Maggie McCausland <maggiemccausland@hotmail.com>
2020-11-06 09:34:25 -0700

    added updated capstone project
    updated navigation, made adjustments to slider, made embeded youtube video "lazy load", made adjustments to forms.

--- 2740_capstone/js/script.js
+++ 2740_capstone/js/script.js
@@ -1,3 +1,9 @@
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("hide");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+    document.getElementById("primaryNav").classList.toggle("open");
+
 }
+
+var x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu
