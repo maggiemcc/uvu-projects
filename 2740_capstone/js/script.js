97474838a38eec27bd9b3fb21a9d1bcaa9805ca1
Maggie McCausland <maggiemccausland@hotmail.com>
2020-11-06 10:17:04 -0700

    made form and video changes

--- 2740_capstone/js/script.js
+++ 2740_capstone/js/script.js
@@ -6,4 +6,34 @@ function toggleMenu() {
 }
 
 var x = document.getElementById('hamburgerBtn');
-x.onclick = toggleMenu
+x.onclick = toggleMenu;
+
+
+
+// VIDEO: Image click loads youtube video
+( function() {
+
+	var youtube = document.querySelectorAll( ".youtubeVideo" );
+	
+	for (var i = 0; i < youtube.length; i++) {
+		
+		var source = "https://img.youtube.com/vi/"+ youtube[i].dataset.embed +"/sddefault.jpg";
+		
+		var image = new Image();
+				image.src = source;
+				image.addEventListener( "load", function() {
+					youtube[ i ].appendChild( image );
+				}( i ) );
+		
+				youtube[i].addEventListener( "click", function() {
+
+					var iframe = document.createElement( "iframe" );
+							iframe.setAttribute( "allowfullscreen", "" );
+							iframe.setAttribute( "src", "https://www.youtube.com/embed/"+ this.dataset.embed +"?rel=0&showinfo=0&autoplay=1" );
+
+							this.innerHTML = "";
+							this.appendChild( iframe );
+				} );	
+	};
+	
+} )();
